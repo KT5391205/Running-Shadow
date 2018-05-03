18fead3db6c49cdb6f4706ae4791d065747cc718
Jojo <32402569+KT5391205@users.noreply.github.com>
2018-05-02 17:03:01 -0700

    aaaaaaaaaaahhh

--- Running Shadow/Player.java
+++ Running Shadow/Player.java
@@ -1,4 +1,13 @@
+import java.util.Scanner;
 public class Player extends Character
 {
-    
+    public void nameSelect()
+    {
+        String name = "";
+        while(name == "")
+        {
+            System.out.println("What is your name?");
+            
+        }
+    }
 }
